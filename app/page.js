fd23e1849f0e37a21111df96a9814f382a316f42
GOGA-23 <gokulgandhi97@gmail.com>
2025-07-18 14:34:15 +0530

    update

--- app/page.js
+++ app/page.js
@@ -5,7 +5,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <Hero home />
+      {/* <Hero home /> */}
+      <div className="w-full min-h-screen flex justify-center items-center">
+        <Image
+          alt="echelon gif"
+          src={"/eshelon.gif"}
+          width={900}
+          height={900}
+          unoptimized
+          className="object-contain object-center"
+        />
+      </div>
     </main>
   );
 }
